741f7d0d5822977da2e25b6c3ee94801a1fc5c5a
dalbizurez <dalbizurez@correo.url.edu.gt>
2025-08-18 01:56:27 -0600

    fix(Components): added main component to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Display from './Components/Display.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Display />
   </StrictMode>,
 )
