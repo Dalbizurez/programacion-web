21e230c54478f73380909ef016e9f792ae0c4d4e
dalbizurez <dalbizurez@correo.url.edu.gt>
2025-08-23 20:48:27 -0600

    feat(Display): added 3 card display system, allowing for (ideally) full evolution chain to be displayed, and added the corresponding css to make it prettier

--- src/Components/Display.jsx
+++ src/Components/Display.jsx
@@ -5,20 +5,22 @@ function Display(){
   const [count, setCount] = useState(1);
 
   const previous = () => {
-    setCount(prev => (prev <= 1 ? 1025 : prev - 1));
+    setCount(prev => (prev <= 1 ? 1025 : prev - 3));
   }
 
   const next = () => {
-    setCount(prev => (prev >= 1025 ? 1 : prev + 1));
+    setCount(prev => (prev >= 1025 ? 1 : prev + 3));
   }
 
 
     return (
         <>
-        <div>
+        <div className="carousel">
             <button onClick={previous}>Prev</button>
 
             <Card index={count} />
+            <Card index={count+1} />
+            <Card index={count+2} />
 
             <button onClick={next}>Next</button>
         </div>
