fec111167414d991bc7e6c6c961d2a1a2757a6ab
dalbizurez <dalbizurez@correo.url.edu.gt>
2025-08-26 09:35:02 -0600

    feat(TaskList): added filter button and it's implementation via the setFilter hook

--- src/Components/TaskList.jsx
+++ src/Components/TaskList.jsx
@@ -3,18 +3,22 @@ import { useState } from "react";
 
 function TaskList({ tasks }) {
     const [taskList, setTaskList] = useState(tasks);
+    const [filter, setFilter] = useState("all");
 
     const handleDelete = (task) => {
-        taskList.splice(taskList.indexOf(task), 1);
-        setTaskList([...taskList]);
-    };
+        tasks.splice(tasks.indexOf(task), 1);
+        setTaskList([...tasks]);
+    }
 
     return (
         <div className="TaskList">
+            <button onClick={() => filterTasks(true)}>Completed</button>
+            <button onClick={() => filterTasks(false)}>Pending</button>
+            <button onClick={() => {setTaskList(tasks); setFilter("all")}}>All</button>
             {taskList.map(task => {
                 return (
                     <div key={task.id} className="task">
-                        <Task_Component id={task.id} name={task.name} completed={task.completed} />
+                        <Task_Component task={task} />
                         <button onClick={() => handleDelete(task)}>Delete</button>
                     </div>
                 );
