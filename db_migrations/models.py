84f8ff45a7d9bc865156f854dc3421f8131f5bf3
dalbizurez <dalbizurez@correo.url.edu.gt>
2025-09-09 19:05:13 -0600

    feat(db_migrtations): Author model added and author field from Book model changed to foreign key

--- db_migrations/models.py
+++ db_migrations/models.py
@@ -5,7 +5,12 @@ from django.db import models
 class Book(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
+    author = models.ForeignKey('Author', on_delete=models.DO_NOTHING)
     genre = models.CharField(max_length=100)
     publisher = models.CharField(max_length=255)
     number_of_pages = models.IntegerField()
+
+class Author(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
