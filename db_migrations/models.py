a1763f3ba911b0feda89a3069d655b6991e9f86a
dalbizurez <dalbizurez@correo.url.edu.gt>
2025-09-09 19:08:46 -0600

    feat(db_migrtations): Genre model added and genre field from Book model changed into foreign key

--- db_migrations/models.py
+++ db_migrations/models.py
@@ -6,7 +6,7 @@ class Book(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     author = models.ForeignKey('Author', on_delete=models.DO_NOTHING)
-    genre = models.CharField(max_length=100)
+    genre = models.ForeignKey('Genre', on_delete=models.DO_NOTHING)
     publisher = models.CharField(max_length=255)
     number_of_pages = models.IntegerField()
 
@@ -14,3 +14,8 @@ class Author(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
+
+class Genre(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
