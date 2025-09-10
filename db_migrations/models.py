e6e05f6ee432b15ff0b50327b4d841840e237f42
dalbizurez <dalbizurez@correo.url.edu.gt>
2025-09-09 18:47:10 -0600

    feat(db_migrations): Book model implemented and registered to admin site

--- db_migrations/models.py
+++ db_migrations/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    genre = models.CharField(max_length=100)
+    publisher = models.CharField(max_length=255)
+    number_of_pages = models.IntegerField()
