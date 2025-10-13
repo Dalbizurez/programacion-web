66cdb954b884e233fe55ca2230070eb1a5a2e6f3
dalbizurez <dalbizurez@correo.url.edu.gt>
2025-10-12 18:54:04 -0600

    feat(SecretBox): Removed form and submit action from button, as we don't want to change/refresh the page

--- frontend/src/components/Secretbox.jsx
+++ frontend/src/components/Secretbox.jsx
@@ -3,11 +3,9 @@
 export default function SecretBox( { onClick } ) {
     return (
         <div className="secret-box">
-            <form action="" method="post">
-                <input type="text" name="secret" placeholder="Enter your secret" />
-                <br />
-                <button onClick={onClick} >Save secret</button>
-            </form>
+            <input type="text" name="secret" id="secret" placeholder="Enter your secret" />
+            <br />
+            <button onClick={onClick} >Save secret</button>
         </div>
     );
 }
