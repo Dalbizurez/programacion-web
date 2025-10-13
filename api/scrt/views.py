ba090b97a84d3f6c5d060b85bbaeb60e8d609def
dalbizurez <dalbizurez@correo.url.edu.gt>
2025-10-12 23:14:08 -0600

    feat(Api): secret encryption implementation

--- api/scrt/views.py
+++ api/scrt/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from cryptography.fernet import Fernet
+from datetime import datetime
 
 # Create your views here.
+
+@csrf_exempt
+def encrypt(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body)
+            secret = data.get('secret')
+
+            date = datetime.now()
+
+            key = Fernet.generate_key()
+            suite = Fernet(key)
+            encrypted = suite.encrypt(secret.encode())
+
+            # encrypted is to be stored in Redis
+
+            return JsonResponse({'key': key.decode()})
+        except Exception as e:
+            print(e)
+            return JsonResponse({'error': str(e)}, status=400)
+    return JsonResponse({'error': 'Invalid request method'}, status=405)
+
