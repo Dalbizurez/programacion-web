02484ad4041ceaab724e00494708aa3fd4cf25f4
dalbizurez <dalbizurez@correo.url.edu.gt>
2025-10-12 23:41:15 -0600

    feat(api): redi settings

--- api/api/settings.py
+++ api/api/settings.py
@@ -128,3 +128,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
