d95a1eca153f86c202a746929bafb2efc479de9d
dalbizurez <dalbizurez@correo.url.edu.gt>
2025-10-12 23:42:47 -0600

    feat(RevealTab): api connection implementation

--- frontend/src/components/Reveal Secret/RevealTab.jsx
+++ frontend/src/components/Reveal Secret/RevealTab.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import KeyBox from "./KeyBox";
 import Secret from "./Secret";
@@ -6,9 +6,24 @@ import Secret from "./Secret";
 export default function RevealTab(){
     const [secret, setSecret] = useState("");
 
-    function getSecretFromApi(){
+
+    async function getSecretFromApi(){
         const key = document.getElementById("keybox").value;
-        if (key) setSecret("shhhhh");
+        if (key) {
+            const response = await fetch(`http://localhost:8000/decrypt/?k=${key}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            const data = await response.json();
+            if (response.ok) {
+                setSecret(data.secret);
+            } else {
+                alert( "No secret found" );
+                console.error('Error decrypting secret:', data);
+            }
+        }
     }
 
     function switchComponent() {
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,6 @@ import TabController from './components/TabController.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
     <TabController />
   </StrictMode>,
 )
